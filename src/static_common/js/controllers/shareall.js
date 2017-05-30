e7b30c853c424dce787196151012f00d18a5f174
Siyang Wang <siyang.rinpoche@gmail.com>
2017-05-30 16:02:56 -0400

    combine the backend codes

--- src/static_common/js/controllers/shareall.js
+++ src/static_common/js/controllers/shareall.js
@@ -137,11 +137,12 @@ controller('ShareAllController',
             angular.forEach(enhancement_all, function(value, index) {
 
                 console.log("enhancement value = "+JSON.stringify(value));
-                $scope.e_raw = value;
+                $scope.e_raw = value.program.concat(value.competing_programs);
 
                 var enhancement_final_release_time = $scope.e_raw.enhancement_final_release_time;
                 var report_last_edit_time = $scope.e_raw.report_last_edit_time;
 
+                //console.log("$scope.e_raw = "+JSON.stringify($scope.competing_programs));
 
                 var e_array_final = [];
                 var e_array_1 = [];
@@ -156,19 +157,34 @@ controller('ShareAllController',
                 var e_array_10 = [];
                 var e_array_11 = [];
 
-                for (i = 0; i < $scope.e_raw.length; i++) {
-                  e_array_1.push($scope.e_raw['p' + (i === 0 ? '' : i + 1)]);
-                  e_array_2.push($scope.e_raw['c' + (i === 0 ? '' : i + 1)]);
-                  e_array_3.push($scope.e_raw['t' + (i === 0 ? '' : i + 1)]);
-                  e_array_4.push($scope.e_raw['d' + (i === 0 ? '' : i + 1)]);
-                  e_array_5.push($scope.e_raw['r' + (i === 0 ? '' : i + 1)]);
-                  e_array_6.push($scope.e_raw['ex' + (i === 0 ? '' : i + 1)]);
-                  e_array_7.push($scope.e_raw['Intl_transcript' + (i === 0 ? '' : i + 1)]);
-                  e_array_8.push($scope.e_raw['Intl_eng_test' + (i === 0 ? '' : i + 1)]);
-                  e_array_9.push($scope.e_raw['s' + (i === 0 ? '' : i + 1)]);
-                  e_array_10.push($scope.e_raw['dura' + (i === 0 ? '' : i + 1)]);
+                // for (i = 0; i < $scope.e_raw.length; i++) {
+                //   e_array_1.push($scope.e_raw['p' + (i === 0 ? '' : i + 1)]);
+                //   e_array_2.push($scope.e_raw['c' + (i === 0 ? '' : i + 1)]);
+                //   e_array_3.push($scope.e_raw['t' + (i === 0 ? '' : i + 1)]);
+                //   e_array_4.push($scope.e_raw['d' + (i === 0 ? '' : i + 1)]);
+                //   e_array_5.push($scope.e_raw['r' + (i === 0 ? '' : i + 1)]);
+                //   e_array_6.push($scope.e_raw['ex' + (i === 0 ? '' : i + 1)]);
+                //   e_array_7.push($scope.e_raw['Intl_transcript' + (i === 0 ? '' : i + 1)]);
+                //   e_array_8.push($scope.e_raw['Intl_eng_test' + (i === 0 ? '' : i + 1)]);
+                //   e_array_9.push($scope.e_raw['s' + (i === 0 ? '' : i + 1)]);
+                //   e_array_10.push($scope.e_raw['dura' + (i === 0 ? '' : i + 1)]);
 
-                }
+                // }
+
+                for(i=0; i<$scope.e_raw.length; i++){
+
+                 e_array_1.push($scope.e_raw[i]['program_detail']);
+                 e_array_2.push($scope.e_raw[i]['curriculum']);
+                 e_array_3.push($scope.e_raw[i]['tuition']);
+                 e_array_4.push($scope.e_raw[i]['deadline']);
+                 e_array_5.push($scope.e_raw[i]['requirement']);
+                 e_array_6.push($scope.e_raw[i]['required_exam']);
+                 e_array_7.push($scope.e_raw[i]['intl_transcript']);
+                 e_array_8.push($scope.e_raw[i]['intl_eng_test']);
+                 e_array_9.push($scope.e_raw[i]['scholarship']);
+                 e_array_10.push($scope.e_raw[i]['duration']);
+
+             }
 
                 e_array_11.push(
                 {
@@ -180,9 +196,7 @@ controller('ShareAllController',
 
                 e_array_final = [e_array_1, e_array_2, e_array_3, e_array_4, e_array_5, e_array_6, e_array_7, e_array_8, e_array_9, e_array_10, e_array_11];
                 $scope.enhancement_array.push(e_array_final)
-                //console.log("e_array_final.program_name="+JSON.stringify(e_array_final[0][0].program_name));
-                //console.log("e_array_final.degree="+JSON.stringify(e_array_final[0][0].degree.name));
-
+               
             });
 
 
