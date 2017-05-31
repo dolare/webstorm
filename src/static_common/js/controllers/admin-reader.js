dde49086f8c3f78c947369ba999a0a82c3ab084d
Siyang Wang <siyang.rinpoche@gmail.com>
2017-05-31 11:08:05 -0400

    fixed admin report preview

--- src/static_common/js/controllers/admin-reader.js
+++ src/static_common/js/controllers/admin-reader.js
@@ -159,7 +159,7 @@ angular.module('myApp').controller('AdminReaderController', ['authenticationSvc'
             
 
              console.log("released report whoops"+ JSON.stringify(response.data));
-             $scope.e_raw = response.data;
+             $scope.e_raw = response.data.program.concat(response.data.competing_programs);
              
              $scope.report = {};
              var e_array_1 = [];
@@ -173,20 +173,36 @@ angular.module('myApp').controller('AdminReaderController', ['authenticationSvc'
              var e_array_9 = [];
              var e_array_10 = [];
 
-             for(i=0; i<$scope.e_raw.length; i++)
-             {
-               e_array_1.push($scope.e_raw['p'+(i===0?'':i+1)]);
-               e_array_2.push($scope.e_raw['c'+(i===0?'':i+1)]);
-               e_array_3.push($scope.e_raw['t'+(i===0?'':i+1)]);
-               e_array_4.push($scope.e_raw['d'+(i===0?'':i+1)]);
-               e_array_5.push($scope.e_raw['r'+(i===0?'':i+1)]);
-               e_array_6.push($scope.e_raw['ex'+(i===0?'':i+1)]);
-               e_array_7.push($scope.e_raw['Intl_transcript'+(i===0?'':i+1)]);
-               e_array_8.push($scope.e_raw['Intl_eng_test'+(i===0?'':i+1)]);
-               e_array_9.push($scope.e_raw['s'+(i===0?'':i+1)]);
-               e_array_10.push($scope.e_raw['dura'+(i===0?'':i+1)]);
+             // for(i=0; i<$scope.e_raw.length; i++)
+             // {
+             //   e_array_1.push($scope.e_raw['p'+(i===0?'':i+1)]);
+             //   e_array_2.push($scope.e_raw['c'+(i===0?'':i+1)]);
+             //   e_array_3.push($scope.e_raw['t'+(i===0?'':i+1)]);
+             //   e_array_4.push($scope.e_raw['d'+(i===0?'':i+1)]);
+             //   e_array_5.push($scope.e_raw['r'+(i===0?'':i+1)]);
+             //   e_array_6.push($scope.e_raw['ex'+(i===0?'':i+1)]);
+             //   e_array_7.push($scope.e_raw['Intl_transcript'+(i===0?'':i+1)]);
+             //   e_array_8.push($scope.e_raw['Intl_eng_test'+(i===0?'':i+1)]);
+             //   e_array_9.push($scope.e_raw['s'+(i===0?'':i+1)]);
+             //   e_array_10.push($scope.e_raw['dura'+(i===0?'':i+1)]);
+
+             // }
+
+             for(i=0; i<$scope.e_raw.length; i++){
+
+                 e_array_1.push($scope.e_raw[i]['program_detail']);
+                 e_array_2.push($scope.e_raw[i]['curriculum']);
+                 e_array_3.push($scope.e_raw[i]['tuition']);
+                 e_array_4.push($scope.e_raw[i]['deadline']);
+                 e_array_5.push($scope.e_raw[i]['requirement']);
+                 e_array_6.push($scope.e_raw[i]['required_exam']);
+                 e_array_7.push($scope.e_raw[i]['intl_transcript']);
+                 e_array_8.push($scope.e_raw[i]['intl_eng_test']);
+                 e_array_9.push($scope.e_raw[i]['scholarship']);
+                 e_array_10.push($scope.e_raw[i]['duration']);
 
              }
+             
 
              $scope.report.e_array_final = [e_array_1, e_array_2, e_array_3, e_array_4, e_array_5, e_array_6, e_array_7, e_array_8, e_array_9, e_array_10];
              
