93173193ef8a035293ea5b7d68e327197a3bcc65
Kaiying <ckykokoko@gmail.com>
2017-06-30 11:12:45 -0400

    Fixed API URL

--- src/apps/upgrid/non_degree_api/urls.py
+++ src/apps/upgrid/non_degree_api/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     # api
     url(r'^schools$', UniversitySchoolListAPI.as_view(), name="schools"),
     url(r'^release_reports$', ReleaseReportCreateListAPI.as_view(), name="release_reports"),
-    url(r'^release_report/(?P<object_id>[0-9a-fA-F\-]+)$', ReleaseReportAPI.as_view(), name="release_report"),
-    url(r'^release_report/over_view/(?P<new_report_id>[0-9a-fA-F\-]+)/(?P<old_report_id>[0-9a-fA-F\-]+)$',
+    url(r'^release_reports/(?P<object_id>[0-9a-fA-F\-]+)$', ReleaseReportAPI.as_view(), name="release_report"),
+    url(r'^release_reports/over_view/(?P<new_report_id>[0-9a-fA-F\-]+)/(?P<old_report_id>[0-9a-fA-F\-]+)$',
         ReportOverview.as_view(), name="release_report_overview"),
 
 ]
