983ed9391b43147f0fa973117435711e6aa1bd64
Siyang Wang <siyang.rinpoche@gmail.com>
2017-07-03 10:18:31 -0400

    added the e edu page

--- src/static_common/js/controllers/admin-executive.js
+++ src/static_common/js/controllers/admin-executive.js
@@ -7,23 +7,23 @@ angular.module('myApp').controller('ExecutiveController', ['$sce', '$q', '$http'
    
     var token = authenticationSvc.getUserInfo().accessToken;
 
-   // $http({
-   //        url: '/api/upgrid/non_degree/schools/',
-   //        method: 'GET',
-   //        headers: {
-   //          'Authorization': 'JWT ' + token
-   //        }
-   //  }).then(function (response) {
+   $http({
+          url: '/api/upgrid/non_degree/schools/',
+          method: 'GET',
+          headers: {
+            'Authorization': 'JWT ' + token
+          }
+    }).then(function (response) {
 
       
 
-   //     console.log("return data"+ JSON.stringify(response.data));
+       console.log("return data"+ JSON.stringify(response.data));
        
-   //  }).
-   //   catch(function(error){
-   //      console.log('an error occurred...'+JSON.stringify(error));
+    }).
+     catch(function(error){
+        console.log('an error occurred...'+JSON.stringify(error));
 
-   //   });
+     });
 
 
 
