8337b1ec66c6dbd3808b47352251c38ccc000691
Kaiying <ckykokoko@gmail.com>
2017-07-15 18:02:46 -0400

    add AMP filter in school API

--- src/apps/upgrid/non_degree_api/filter.py
+++ src/apps/upgrid/non_degree_api/filter.py
@@ -10,6 +10,7 @@ from ..models import NonDegreeReport
 class UniversitySchoolFilter(FilterSet):
     university_id = django_filters.UUIDFilter(name="university_foreign_key__object_id")
     is_non_degree = django_filters.BooleanFilter(method="non_degree_filter")
+    is_AMP = django_filters.BooleanFilter(name="nondegreecategory__nondegreecourse__is_advanced_management_program")
     client_id = django_filters.UUIDFilter(name="non_degree_user__id")
 
     class Meta:
@@ -20,6 +21,11 @@ class UniversitySchoolFilter(FilterSet):
         if value is True:
             return queryset.filter(nondegreecategory__isnull=False).distinct()
         return queryset
+
+    # def AMP_filter(self, queryset, name, value):
+    #     if value is True:
+    #         return queryset.filter(nondegreecategory__nondegreecourse__is_advanced_management_program=False).distinct()
+    #     return queryset
 
 
 class ReportFilter(FilterSet):
