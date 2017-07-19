79beaaf9137331af868a2f2947456880507d5c21
Jack Tan <jack.ziqi.tan@gmail.com>
2017-07-19 16:13:47 -0400

    Added currency symbols to Exec Edu admin controller js.

--- src/static_common/js/controllers/non_degree.js
+++ src/static_common/js/controllers/non_degree.js
@@ -42,6 +42,7 @@ controller('NonDegreeController', function($scope, $http, authenticationSvc, $lo
       'UAH': '₴', // Ukrainian Hryvnia
       'VND': '₫', // Vietnamese Dong
       'CNY': '¥', // Chinese Yuan
+      'null': '$', // The default currency sign is USD
     };
 
   $scope.check_storage = function(){
