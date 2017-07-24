b5f2758af709dbf518a32f4cc88175ff48b43773
Kaiying <ckykokoko@gmail.com>
2017-07-24 13:19:21 -0400

    Added non-degree filter in university customer API

--- src/apps/upgrid/filter.py
+++ src/apps/upgrid/filter.py
@@ -6,6 +6,8 @@ from .models import UniversityCustomer, ClientAndProgramRelation
 
 class UniversityCustomerFilter(FilterSet):
     main_user_id = django_filters.UUIDFilter(method='main_user_filter')
+    is_main_user = django_filters.BooleanFilter(method='is_main_user_filter')
+    is_non_degree_user = django_filters.BooleanFilter(method='is_non_degree_user_filter')
 
     class Meta:
         model = UniversityCustomer
@@ -13,6 +15,16 @@ class UniversityCustomerFilter(FilterSet):
 
     def main_user_filter(self, queryset, name, value):
         return queryset.filter(main_user_id=str(value))
+
+    def is_main_user_filter(self, queryset, name, value):
+        if value is True:
+            return queryset.filter(account_type='main')
+        return queryset.filter(account_type='sub')
+
+    def is_non_degree_user_filter(self, queryset, name, value):
+        if value is True:
+            return queryset.exclude(non_degree_schools=None)
+        return queryset.filter(non_degree_schools=None)
 
 
 class ClientAndProgramRelationFilter(FilterSet):
