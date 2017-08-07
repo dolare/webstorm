32e454f12633e2e6ffd808f3bf8bdde2c733f9d0
dolarebrave@gmail.com <dolarebrave@gmail.com>
2017-08-07 11:37:13 -0400

    clean the code

--- src/apps/upgrid/api_report_history.py
+++ src/apps/upgrid/api_report_history.py
@@ -1,26 +1,34 @@
 from rest_framework import generics, mixins
+from rest_framework import permissions
 from .api_serializers_report_history import (whoopsReportHistorySerializer,enhancementReportHistorySerializer)
 from .pagination import CustomerPageNumberPagination
 
 class whoopsReportHistoryList(generics.ListAPIView):
-    serializer_class = whoopsReportHistorySerializer
-    pagination_class = CustomerPageNumberPagination
-    multiple_lookup_fields = ['customer_program','customer',]
+	permission_classes = (permissions.IsAuthenticated,)
+	serializer_class = whoopsReportHistorySerializer
+	pagination_class = CustomerPageNumberPagination
+	multiple_lookup_fields = ['customer_program','customer',]
 
 
 
 class enhancementReportHistoryList(generics.ListAPIView):
-    serializer_class = enhancementReportHistorySerializer
-    pagination_class = CustomerPageNumberPagination
+	permission_classes = (permissions.IsAuthenticated,)
 
-    multiple_lookup_fields = ['customer_program','customer',]
+	serializer_class = enhancementReportHistorySerializer
+	pagination_class = CustomerPageNumberPagination
+
+	multiple_lookup_fields = ['customer_program','customer',]
 
 
 
 
 class whoopsReportHistory(generics.UpdateAPIView):
+	permission_classes = (permissions.IsAuthenticated,)
+
 	serializer_class = whoopsReportHistorySerializer
 
 
 class enhancementReportHistory(generics.UpdateAPIView):
+	permission_classes = (permissions.IsAuthenticated,)
+
 	serializer_class = enhancementReportHistorySerializer
