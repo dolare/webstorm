acd2e1bbde3ad8bd82111cf95b24594375d82a4d
Jack Tan <jack.ziqi.tan@gmail.com>
2017-08-29 16:16:08 -0400

    Fixed defect of EDR in Exec Ed Comparison Tool.

--- src/static_common/js/controllers/comparison.js
+++ src/static_common/js/controllers/comparison.js
@@ -2,161 +2,161 @@ var comparison = angular.module('myApp')
 comparison.controller('ComparisonController',
   function($scope, $http, authenticationSvc, avatarService, $timeout) {
 
-  	var token = authenticationSvc.getUserInfo().accessToken;
-    var avatar_value = avatarService.getClientId() ? avatarService.getClientId()+'/' : "";
+    var token = authenticationSvc.getUserInfo().accessToken;
+    var avatar_value = avatarService.getClientId() ? avatarService.getClientId() + '/' : "";
 
-    
-    
+
+
     // $timeout( function(){
     //         jQuery('.comparison_height').scrollLock();
     //     }, 5000 );
 
-  	 $http({
-          url: '/api/upgrid/non_degree/schools?page_size=100',
-          method: 'GET',
-          headers: {
-            'Authorization': 'JWT ' + token
-          }
-    }).then(function (response) {
+    $http({
+      url: '/api/upgrid/non_degree/schools?page_size=100',
+      method: 'GET',
+      headers: {
+        'Authorization': 'JWT ' + token
+      }
+    }).then(function(response) {
 
-       $scope.schools = response.data.results
+      $scope.schools = response.data.results
 
-       console.log("return data"+ JSON.stringify($scope.schools, null, 4));
-       
-    }).
-     catch(function(error){
-        console.log('an error occurred...'+JSON.stringify(error));
-
-     });
-
-
-
-
-     $scope.get_cat1 = function(school_id){
-
-     	if(school_id) {
-     		$http({
-          url: '/api/upgrid/non_degree/schools/'+ school_id +  '/categories',
-          method: 'GET',
-          headers: {
-            'Authorization': 'JWT ' + token
-          }
-    }).then(function (response) {
-
-       $scope.cat1 = response.data
-
-       console.log("return cat"+ JSON.stringify($scope.cat1, null, 4));
-       
-       
+      console.log("return data" + JSON.stringify($scope.schools, null, 4));
 
     }).
-     catch(function(error){
-        console.log('an error occurred...'+JSON.stringify(error));
+    catch(function(error) {
+      console.log('an error occurred...' + JSON.stringify(error));
 
-     });
-
-     	} else {
-     		$scope.cat1 = null
-     		$scope.courses1 = null
-     	}
-     	
-     	
-     }
+    });
 
 
-     $scope.get_cat2 = function(school_id){
-     	
-     	if(school_id){
-     		$http({
-          url: '/api/upgrid/non_degree/schools/'+ school_id +  '/categories',
+
+
+    $scope.get_cat1 = function(school_id) {
+
+      if (school_id) {
+        $http({
+          url: '/api/upgrid/non_degree/schools/' + school_id + '/categories',
           method: 'GET',
           headers: {
             'Authorization': 'JWT ' + token
           }
-    }).then(function (response) {
+        }).then(function(response) {
 
-       $scope.cat2 = response.data
+          $scope.cat1 = response.data
 
-       console.log("return cat"+ JSON.stringify($scope.cat2, null, 4));
-       
-    }).
-     catch(function(error){
-        console.log('an error occurred...'+JSON.stringify(error));
-
-     });
-     	} else {
-     		$scope.cat2 = null
-     		$scope.courses2 = null
-     	}
-     	
-
-     }
+          console.log("return cat" + JSON.stringify($scope.cat1, null, 4));
 
 
 
-     $scope.get_courses1 = function(school_id, cat_id) {
-     	
-     	console.log("school_id="+school_id+" cat_id="+cat_id)
+        }).
+        catch(function(error) {
+          console.log('an error occurred...' + JSON.stringify(error));
 
-     	$http({
-          url: '/api/upgrid/non_degree/schools/'+ school_id +  '/categories/' + cat_id + '/courses',
+        });
+
+      } else {
+        $scope.cat1 = null
+        $scope.courses1 = null
+      }
+
+
+    }
+
+
+    $scope.get_cat2 = function(school_id) {
+
+      if (school_id) {
+        $http({
+          url: '/api/upgrid/non_degree/schools/' + school_id + '/categories',
           method: 'GET',
           headers: {
             'Authorization': 'JWT ' + token
           }
-	    }).then(function (response) {
+        }).then(function(response) {
 
-	       $scope.courses1 = response.data;
+          $scope.cat2 = response.data
 
-         angular.forEach($scope.courses1, function(course, key) {
-          if (course.course_dates.length > 0 && course.tuition_number && !course.is_advanced_management_program) 
+          console.log("return cat" + JSON.stringify($scope.cat2, null, 4));
+
+        }).
+        catch(function(error) {
+          console.log('an error occurred...' + JSON.stringify(error));
+
+        });
+      } else {
+        $scope.cat2 = null
+        $scope.courses2 = null
+      }
+
+
+    }
+
+
+
+    $scope.get_courses1 = function(school_id, cat_id) {
+
+      console.log("school_id=" + school_id + " cat_id=" + cat_id)
+
+      $http({
+        url: '/api/upgrid/non_degree/schools/' + school_id + '/categories/' + cat_id + '/courses',
+        method: 'GET',
+        headers: {
+          'Authorization': 'JWT ' + token
+        }
+      }).then(function(response) {
+
+        $scope.courses1 = response.data;
+
+        angular.forEach($scope.courses1, function(course, key) {
+          if (course.course_dates.length > 0 && course.tuition_number && !course.is_advanced_management_program)
             course.edr = getEDR(course.course_dates[0].start_date, course.course_dates[0].end_date, course.tuition_number);
           else
             course.edr = null;
-         });
+        });
 
-	       console.log("return courses"+ JSON.stringify($scope.courses1, null, 4));
-	       
-	    }).
-	     catch(function(error){
-	        console.log('an error occurred...'+JSON.stringify(error));
+        console.log("return courses" + JSON.stringify($scope.courses1, null, 4));
 
-	     });
+      }).
+      catch(function(error) {
+        console.log('an error occurred...' + JSON.stringify(error));
 
-     }
+      });
+
+    }
 
 
 
-     $scope.get_courses2 = function(school_id, cat_id) {
-     	
-     	console.log("school_id="+school_id+" cat_id="+cat_id)
+    $scope.get_courses2 = function(school_id, cat_id) {
 
-     	$http({
-          url: '/api/upgrid/non_degree/schools/'+ school_id +  '/categories/' + cat_id + '/courses',
-          method: 'GET',
-          headers: {
-            'Authorization': 'JWT ' + token
-          }
-	    }).then(function (response) {
+      console.log("school_id=" + school_id + " cat_id=" + cat_id)
 
-	       $scope.courses2 = response.data;
+      $http({
+        url: '/api/upgrid/non_degree/schools/' + school_id + '/categories/' + cat_id + '/courses',
+        method: 'GET',
+        headers: {
+          'Authorization': 'JWT ' + token
+        }
+      }).then(function(response) {
 
-         angular.forEach($scope.courses2, function(course, key) {
-          if (course.course_dates.length > 0 && course.tuition_number && !course.is_advanced_management_program) 
+        $scope.courses2 = response.data;
+
+        angular.forEach($scope.courses2, function(course, key) {
+          if (course.course_dates.length > 0 && course.tuition_number && !course.is_advanced_management_program)
             course.edr = getEDR(course.course_dates[0].start_date, course.course_dates[0].end_date, course.tuition_number);
           else
             course.edr = null;
-         });
+        });
 
-	       console.log("return courses"+ JSON.stringify($scope.courses2, null, 4));
-	       
-	    }).
-	     catch(function(error){
-	        console.log('an error occurred...'+JSON.stringify(error));
+        console.log("return courses" + JSON.stringify($scope.courses2, null, 4));
 
-	     });
+      }).
+      catch(function(error) {
+        console.log('an error occurred...' + JSON.stringify(error));
 
-     }
+      });
+
+    }
 
     // Function to calculate EDR (Equivalent Daily Rate), given the course start date, end date and the amount of tuition
     var getEDR = function(startDate, endDate, tuition) {
