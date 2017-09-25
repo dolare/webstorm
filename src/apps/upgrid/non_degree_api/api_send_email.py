204e0f3b60bcafc4d87acf1a96b678e86cbef347
dolarebrave@gmail.com <dolarebrave@gmail.com>
2017-09-25 16:21:08 -0400

    send email 08

--- src/apps/upgrid/non_degree_api/api_send_email.py
+++ src/apps/upgrid/non_degree_api/api_send_email.py
@@ -31,8 +31,78 @@ class SendNotification(APIView):
 
     def post(self, request, *args, **kwargs):
 
-        query_set = NonDegreeReportCustomerMapping.objects.filter(is_sent = False).order_by('-date_modified','report').distinct('date_modified','report')
+        query_set = NonDegreeReportCustomerMapping.objects.filter(is_sent = False, customer__active == True).order_by('-date_modified','report').distinct('date_modified','report')
         send_list = {}
+        for query in query_set:
+            if query.customer.email in send_list.keys():
+                report_dict = {}
+                report_dict['object_id'] = query.report.object_id
+                report_dict['school_name'] = query.report.school.school
+                report_dict['date_modified'] = query.report.date_modified 
+                send_list[query.customer.email]['report'].append(report_dict)
+            else:
+                send_list[query.customer.email] = {}
+                send_list[query.customer.email]['customer'] = {}
+                send_list[query.customer.email]['customer']['username'] = query.customer.username
+                send_list[query.customer.email]['customer']['school'] = query.customer.Ceeb
+                send_list[query.customer.email]['report'] = []
+                report_dict = {}
+                report_dict['object_id'] = query.report.object_id
+                report_dict['school_name'] = query.report.school.school
+                report_dict['date_modified'] = query.report.date_modified 
+                send_list[query.customer.email]['report'].append(report_dict)
+    
+            cc_addresses = [cc_email]
+            cc_addresses_tuple = tuple(cc_addresses)
+
+        #generate the course and category changes and display as table rows
+        for (customer, content) in send_list.items():
+            html_tr = ''
+            print(content['report'])
+            for report in content['report']:
+                reportTwo = NonDegreeReport.objects.filter(school__school=report['school_name'], active = True, customer__active = True).order_by('-date_created')[:2]
+                report_data = []
+                print(reportTwo)
+                if len(reportTwo) > 0:
+                    for report_obj in reportTwo:
+                        report_data.append(ReportSerializer(report_obj).data)
+
+                    diff_data = ReportOverview.count_diff(report_data[1], report_data[0])
+                    print(diff_data)
+                
+                
+                cr = diff_data['category_removed']
+                ca = diff_data['category_added']
+                cor = diff_data['course_removed']
+                coa = diff_data['course_added']
+                print(report)
+                html_tr = tableRow.format(report['school_name'], report['date_modified'].date(), cr, ca, cor, coa) + html_tr
+
+            print(html_tr)
+            html_content = html.format(customer, html_tr)
+            try:
+                message = EmailMessage(subject='Update Notification', body=html_content, 
+                                        to=[customer], bcc=cc_addresses_tuple)
+                message.content_subtype = 'html'
+                message.send()
+                
+                temp_report_mapping = NonDegreeReportCustomerMapping.objects.filter(customer__email = customer).update(is_sent = True, send_fail = False)
+ 
+            except(BadHeaderError, SMTPServerDisconnected, SMTPSenderRefused, SMTPRecipientsRefused, SMTPDataError,
+                SMTPConnectError, SMTPHeloError, SMTPAuthenticationError) as e:
+                
+                temp_report_mapping = NonDegreeReportCustomerMapping.objects.filter(customer__email = customer).update(is_sent = False, send_fail = True)
+                app_logger.exception('{0} when sending email. Error: {1}'.format(type(e).__name__, html_content))
+               
+                continue
+        
+        return Response({"success": ("email have been sent succussful.")}, status=HTTP_202_ACCEPTED)
+
+
+class PreviewNotification(APIView):
+    def get(self, request, *args, **kwargs):
+        query_set = NonDegreeReportCustomerMapping.objects.filter(is_sent = False)
+                send_list = {}
         for query in query_set:
             if query.customer.email in send_list.keys():
                 report_dict = {}
@@ -80,39 +150,7 @@ class SendNotification(APIView):
 
             print(html_tr)
             html_content = html.format(customer, html_tr)
-            try:
-                message = EmailMessage(subject='Update Notification', body=html_content, 
-                                        to=[customer], bcc=cc_addresses_tuple)
-                message.content_subtype = 'html'
-                message.send()
-                
-                temp_report_mapping = NonDegreeReportCustomerMapping.objects.filter(customer__email = customer).update(is_sent = True, send_fail = False)
- 
-            except(BadHeaderError, SMTPServerDisconnected, SMTPSenderRefused, SMTPRecipientsRefused, SMTPDataError,
-                SMTPConnectError, SMTPHeloError, SMTPAuthenticationError) as e:
-                
-                temp_report_mapping = NonDegreeReportCustomerMapping.objects.filter(customer__email = customer).update(is_sent = False, send_fail = True)
-                app_logger.exception('{0} when sending email. Error: {1}'.format(type(e).__name__, html_content))
-               
-                continue
-        
-        return Response({"success": ("email have been sent succussful.")}, status=HTTP_202_ACCEPTED)
-
-
-class PreviewNotification(APIView):
-    def get(self, request, *args, **kwargs):
-        query_set = NonDegreeReportCustomerMapping.objects.filter(is_sent = False)
-        send_list = {}
-        for query in query_set:
-            if query.customer.email in send_list.keys():
-                send_list[query.customer.email]['report'].append("{},{}".format(query.report.school.school, query.report.categories))
-            else:
-                send_list[query.customer.email] = {}
-                send_list[query.customer.email]['customer'] = {}
-                send_list[query.customer.email]['customer']['username'] = query.customer.username
-                send_list[query.customer.email]['customer']['school'] = query.customer.Ceeb.school
-                send_list[query.customer.email]['report'] = []
-                send_list[query.customer.email]['report'].append("{},{}".format(query.report.school.school, query.report.categories))
+            send_list[query.customer.email]['email_content'] = html_content
             
         return HttpResponse(json.dumps(send_list), status=HTTP_200_OK)
 
