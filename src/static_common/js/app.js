f96ce69fe54a62b131ccf96d4402dea427f407dd
Siyang Wang <siyang.rinpoche@gmail.com>
2017-10-05 16:46:58 -0400

    visualization tool enhancement

--- src/static_common/js/app.js
+++ src/static_common/js/app.js
@@ -1258,6 +1258,7 @@ App.config(function($stateProvider, $urlRouterProvider) {
                             insertBefore: '#css-bootstrap',
                             serie: true,
                             files: [
+                              '/static/js/services/apiService.js',
                               'https://cdnjs.cloudflare.com/ajax/libs/angularjs-dropdown-multiselect/2.0.0-beta.10/angularjs-dropdown-multiselect.min.js',
                               'https://cdnjs.cloudflare.com/ajax/libs/echarts/3.7.1/echarts.min.js',
                               '/static/js/controllers/visualization.js',
