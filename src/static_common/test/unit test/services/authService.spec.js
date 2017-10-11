893c611d41f8d04afbae837d8ca38342e03aa8a9
Chenyuan Wang <wilteadus@gmail.com>
2017-10-11 09:36:47 -0400

    auth service unit test init

--- src/static_common/test/unit test/services/authService.spec.js
+++ src/static_common/test/unit test/services/authService.spec.js
@@ -1,22 +1,39 @@
 describe('Auth Service', function() {
-  var userinfo;
-  var injector;
+  var authService;
+
 
   // Load our api.users module
   beforeEach(angular.mock.module('myApp'));
 
   // Set our injected Users factory (_Users_) to our local Users variable
+  // Inject service into the authService variable 
   beforeEach(function(){
       inject(function($injector){
-        injector = $injector;
+        authService = $injector.get('authenticationSvc');
       })
   });
 
   // A simple test to verify the Users service exists
   it('should exist', function() {
-    //userinfo = injector.get('$factory')('authenticationSvc');
-    userinfo = injector.get('authenticationSvc');
-    expect(userinfo).toBeDefined();
+    expect(authService).toBeDefined();
   });
+
+  describe("Log In method",function(){
+
+    var login,httpBackend;
+    //inject login method into a variable before each test
+    beforeEach(function($httpBackend){
+      login = authService.login;
+      httpBackend = $httpBackend;
+    });
+
+
+      // Test login method is defined or not
+     it('should exist',function(){
+      expect(login).toBeDefined();
+     }); 
+
+     //  
+  })
 
 });
